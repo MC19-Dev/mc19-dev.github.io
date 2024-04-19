77e587c0ab431cd6dd7e171ed9f16e28510f95d4
Madalin <56033676+MC19-Dev@users.noreply.github.com>
2024-04-19 17:54:29 +0300

    Update mdcp_it.js

--- mdcp_it.js
+++ mdcp_it.js
@@ -1,24 +1,23 @@
-allCookies = document.cookie;
-document.cookie = newCookie;
 document.cookie = "session=test GDPR";
 document.cookie = "favorite_task=collect Data";
-function alertCookie() { 
-  alert(document.cookie); 
-} 
 document.cookie = "name=oeschger; SameSite=None; Secure";
 document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+document.cookie = "test1=Hello; SameSite=None; Secure";
+document.cookie = "test2=World; SameSite=None; Secure";
+document.cookie = "reader=1; SameSite=None; Secure";
 
+function alertCookie() { 
+  alert(document.cookie); 
+}
 function showCookies() {
   const output = document.getElementById("cookies");
-  output.textContent = `> ${document.cookie}`;
+  output.textContent = `> ${document.cookie}\n`;
 }
 
 function clearOutputCookies() {
   const output = document.getElementById("cookies");
   output.textContent = "";
 }
-document.cookie = "test1=Hello; SameSite=None; Secure";
-document.cookie = "test2=World; SameSite=None; Secure";
 
 const cookieValue = document.cookie
   .split("; ")
@@ -30,23 +29,18 @@ function showCookieValue() {
   output.textContent = `> ${cookieValue}`;
 }
 
-function clearOutputCookieValue() {
-  const output = document.getElementById("cookie-value");
-  output.textContent = "";
-}
-
 function doOnce() {
   if (
     !document.cookie
       .split("; ")
-      .find((row) => row.startsWith("doSomethingOnlyOnce"))
+      .find((row) => row.startsWith("session"))
   ) {
     // Note that we are setting `SameSite=None;` in this example because the example
     // needs to work cross-origin.
     // It is more common not to set the `SameSite` attribute, which results in the default,
     // and more secure, value of `SameSite=Lax;`
     document.cookie =
-      "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure";
+      "session=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure";
 
     const output = document.getElementById("do-once");
     output.textContent = "> Do something here!";
@@ -64,7 +58,7 @@ function resetOnce() {
   // It is more common not to set the `SameSite` attribute, which results in the default,
   // and more secure, value of `SameSite=Lax;`
   document.cookie =
-    "doSomethingOnlyOnce=; expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=None; Secure";
+    "session=; expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=None; Secure";
 
   const output = document.getElementById("reset-once");
   output.textContent = "> Reset!";
